d4bee8a185569ea0841dd7587775dfe80968a8cc
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-06-01 10:22:01 +0200

    Re-formatting and delete class validators

--- app/dto/area.dto.ts
+++ app/dto/area.dto.ts
@@ -1,19 +1,10 @@
-import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
 import { Area } from '../entities/area.entity';
 import { Device } from '../entities/device.entity';
 
 export class AreaDto {
-
-  @IsString()
-  @IsNotEmpty()
-  @MaxLength(30)
   readonly name!: string;
-
   readonly description!: string;
-
   readonly parent!: Area;
-
   readonly childrens!: Area[];
-
   readonly devices!: Device[];
 }
