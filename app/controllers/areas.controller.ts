28794ca7cceb134f959010e31cd71447bfe7439a
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-06-02 20:49:32 +0200

    Print request url on log

--- app/controllers/areas.controller.ts
+++ app/controllers/areas.controller.ts
@@ -1,8 +1,8 @@
-import { Post, JsonController, Body, Get, Res, Param, Delete } from 'routing-controllers';
+import { Post, JsonController, Body, Get, Res, Param, Delete, Req } from 'routing-controllers';
 import { AreasService } from '../services/areas.service';
 import { AreaDto } from '../dto/area.dto';
 import { Area } from '../entities/area.entity';
-import { Response } from 'express'
+import { Response, Request } from 'express'
 import { Device } from '../entities/device.entity';
 
 
@@ -11,39 +11,39 @@ export class AreasController {
     constructor(private readonly areasService: AreasService) {}
 
     @Post()
-    async createArea(@Body() areaDto: AreaDto, @Res() res: Response) {
-        console.log("Received POST request at /areas endpoint.");
+    async createArea(@Body() areaDto: AreaDto, @Req() req: Request, @Res() res: Response) {
+        console.log("Received POST request at " + req.url + " endpoint.");
         const response = await this.areasService.createArea(areaDto);
         return res.send(response);
     }
 
     @Get('/:slug')
-    async getAreaByslug(@Param('slug') slug: string): Promise<Area> {
-        console.log("Received GET request at /areas/:slug endpoint.");
+    async getAreaByslug(@Param('slug') slug: string, @Req() req: Request): Promise<Area> {
+        console.log("Received GET request at " + req.url + " endpoint.");
         return await this.areasService.getArea(slug);
     }
 
     @Get('/:slug/devices')
-    async getDevicesOfArea(@Param('slug') slug: string): Promise<Device[]> {
-        console.log("Received GET request at /areas/:slug/devices endpoint.");
+    async getDevicesOfArea(@Param('slug') slug: string, @Req() req: Request): Promise<Device[]> {
+        console.log("Received GET request at " + req.url + " endpoint.");
         return await this.areasService.getDevicesOfArea(slug);
     }
 
     @Get('/:slug/parent')
-    async getParentOfArea(@Param('slug') slug: string): Promise<Area> {
-        console.log("Received GET request at /areas/:slug/parent endpoint.");
+    async getParentOfArea(@Param('slug') slug: string, @Req() req: Request): Promise<Area> {
+        console.log("Received GET request at " + req.url + " endpoint.");
         return await this.areasService.getParentOfArea(slug);
     }
 
     @Get('/:slug/childrens')
-    async getChildrensOfArea(@Param('slug') slug: string): Promise<Area[]> {
-        console.log("Received GET request at /areas/:slug/childrens endpoint.");
+    async getChildrensOfArea(@Param('slug') slug: string, @Req() req: Request): Promise<Area[]> {
+        console.log("Received GET request at " + req.url + " endpoint.");
         return await this.areasService.getChildrensOfArea(slug);
     }
 
     @Delete('/:slug')
-    async deleteArea(@Param('slug') slug: string, @Res() res: Response) {
-        console.log("Received DELETE request at /areas/:slug endpoint.");
+    async deleteArea(@Param('slug') slug: string, @Req() req: Request, @Res() res: Response) {
+        console.log("Received DELETE request at " + req.url + " endpoint.");
         const response = await this.areasService.deleteArea(slug);
         return res.send(response);
     }
--- app/controllers/devices.controller.ts
+++ app/controllers/devices.controller.ts
@@ -1,5 +1,5 @@
-import { Post, JsonController, Body, Get, Res, Param, Delete } from 'routing-controllers';
-import { Response } from 'express'
+import { Post, JsonController, Body, Get, Res, Param, Delete, Req } from 'routing-controllers';
+import { Response, Request } from 'express'
 import { DevicesService } from '../services/devices.service'
 import { DeviceDto } from '../dto/device.dto'
 import { Device } from '../entities/device.entity';
@@ -10,22 +10,22 @@ export class DevicesController {
     constructor(private readonly devicesService: DevicesService) {}
 
     @Post()
-    async createDevice(@Body() deviceDto: DeviceDto, @Res() res: Response) {
-        console.log("Received POST request at /devices endpoint.");
+    async createDevice(@Body() deviceDto: DeviceDto, @Req() req: Request, @Res() res: Response) {
+        console.log("Received POST request at " + req.url + " endpoint.");
         const response = await this.devicesService.createDevice(deviceDto);
         return res.send(response);
     }
 
     @Delete('/:slug')
-    async deleteDevice(@Param('slug') slug: string, @Res() res: Response) {
-        console.log("Received DELETE request at /devices:slug endpoint.");
+    async deleteDevice(@Param('slug') slug: string, @Req() req: Request, @Res() res: Response) {
+        console.log("Received DELETE request at " + req.url + " endpoint.");
         const response = await this.devicesService.deleteDevice(slug);
         return res.send(response)
     }
 
     @Get('/:slug')
-    async getDeviceByslug(@Param('slug') slug: string): Promise<Device> {
-        console.log("Received GET request at /devices/:slug endpoint.");
+    async getDeviceByslug(@Param('slug') slug: string, @Req() req: Request): Promise<Device> {
+        console.log("Received GET request at " + req.url + " endpoint.");
         return await this.devicesService.getDevice(slug);
     }
 }
