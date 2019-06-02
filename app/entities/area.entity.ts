ee616836d5fc92c5e1a28e28c1c5cb26a9a7a7eb
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-06-02 18:57:54 +0200

    Use UUID instead of AutoGenerated ID. Insert slug in area and device Id.

--- app/entities/area.entity.ts
+++ app/entities/area.entity.ts
@@ -1,25 +1,20 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import  slug  from 'slug'
 import { Device } from "./device.entity";
 
 @Entity("Areas")
 export class Area {
-    @PrimaryGeneratedColumn()
-    public id!: number;
+    @PrimaryGeneratedColumn("uuid")
+    public uuid!: string;
 
-    @Column({
-        type: 'varchar',
-        length: 50,
-        unique: true,
-    })
+    @Column({ type: 'varchar', length: 150, unique: true})
     public name: string;
 
-    @Column({
-        type: "varchar",
-        length: 150,
-        nullable: true,
-    })
-    public description: string;
+    @Column({ type: 'varchar', length: 150, unique: true})
+    public slug: string;
 
+    @Column({ type: 'varchar', length: 150 })
+    public description: string;
 
     @ManyToOne(type => Area, sector => sector.childrens, { onDelete: 'CASCADE' })
     parent: Area;
@@ -31,11 +26,12 @@ export class Area {
     devices: Device[];
 
     constructor(name: string, description: string, parent: Area, childrens: Area[], devices: Device[]){
-        this.name= name;
-        this.description=description;
-        this.parent=parent;
-        this.childrens=childrens;
-        this.devices=devices;
+        this.name = name;
+        this.slug = slug(this.name);
+        this.description = description;
+        this.parent = parent;
+        this.childrens = childrens;
+        this.devices = devices;
     }
 
 }
